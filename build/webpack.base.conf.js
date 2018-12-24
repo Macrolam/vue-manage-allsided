11342ba641c6ba5ae868834c3cf96548aedccc80
yangguangyu <yangguangyu@yangguangyudeMacBook-Air.local>
2018-12-24 19:58:25 +0800

    init webpack 配置注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,14 +3,12 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-const chalk = require('chalk')
+const chalk = require('chalk')//打包过程中 shell中输出的信息 会彩色打印
 
 function resolve (dir) {
-  return path.join(__dirname, '..', dir)//用于连接路径。该方法的主要用途在于，会正确使用当前系统的路径分隔符，Unix系统是"/"，Windows系统是"\"。
+  return path.join(__dirname, '..', dir)//用于连接路径。这里将三个参数连接在一起:当前路径向上走一级的dir 目录.该方法的主要用途在于，会正确使用当前系统的路径分隔符，Unix系统是"/"，Windows系统是"\"。
 }
 console.log(chalk.red(path.join(__dirname,'..',"dir")));//D:\Acode\self\vue-manage-allsided\dir
-console.log(chalk.red(path.join(__dirname,"dir")));//Acode\self\vue-manage-allsided\build\dir
-console.log(chalk.red("ok"));
 
 
 module.exports = {
@@ -19,17 +17,18 @@ module.exports = {
     app: './src/main.js'
   },
   output: {
-    path: config.build.assetsRoot,
+    path: config.build.assetsRoot,//path.resolve(__dirname, '../dist')
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json'],//可以省略的扩展名.当同一路径下 既有.Vue 又有.js 文件时候 引入时候省略后缀名时默认导入的是js文件.
+    // 别名
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src'),//path.join(__dirname, '..', 'src')  ==> 第一个参数__dirname代表当前文件的位置即: build ;第二个参数: ../;第三个参数: src ;最后连接完:也就是项目根目录下的src目录
     }
   },
   module: {
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,13 +2,13 @@
 const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
-const merge = require('webpack-merge')
-const path = require('path')
+const merge = require('webpack-merge')//合并对象的.
+const path = require('path')//nodeJs 路径模块
 const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const portfinder = require('portfinder')
+const CopyWebpackPlugin = require('copy-webpack-plugin')// 拷贝 项目根目录下的静态资源文件夹static 这里的东西不需要处理只要照着搬到构建好的目录下即可
+const HtmlWebpackPlugin = require('html-webpack-plugin')//该插件将为您生成一个HTML5文件，其中包含打包好的js css文件引入
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')//识别某些类型的webpack错误并清理 在shell 窗口中输出显示  提供更好的开发人员体验
+const portfinder = require('portfinder')//在当前设备上查找可用的开放端口 A simple tool to find an open port on the current machine
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -53,10 +53,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
+      filename: 'index.html',//会按照template给的文件生成一份新的html,文件名这里也叫index . 即:Also generate a index.html
+      template: 'index.html',//需要模板的路径,根目录下 index.html 是模板文件,注有:<!-- built files will be auto injected -->
+      inject: true //传递true或'body'时，所有javascript资源都将放置在body元素的底部。'head'将脚本放在head元素中
     }),
+
     // copy custom static assets
     new CopyWebpackPlugin([
       {
